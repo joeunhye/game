ca4edc58bf121f085f9f13ea4e0f67029854012d
Eunhye Jo <eunhye919@vtex.co.kr>
2020-03-25 17:09:47 +0900

    2020.03.25

--- game.js
+++ game.js
@@ -15,78 +15,82 @@ let btIndex1;
 let btIndex2;
 
 $(document).ready(function () {
-	vegas.gameS(); //game start
-	
+	vegas.gameS(); //game start!
 });
 
 
+// 카지노 배팅
+function bettingS(bettingTotal) {
+	$('.player .score').text(bettingTotal); //최초 금액 배당 표시
 
-function bettingS(bettingTotal,player) {
-	
-	$('.player .score').text(bettingTotal); // 최초 배당
-
-	//플레이어 배팅 진행
+	// 플레이어 배팅 진행
 	$('.gAreaIn li').click(function() {
 
-		$(this).parent().addClass('start'); // 배팅 시작 유무 판별 클래스
-		//$(this).append('<span class="btPlayer">배팅!'+ 1 + '</span>');
+		$(this).parent().addClass('start'); //배팅 시작 유무 판별 클래스
+		bettingN = $(this).index()+1; // 배팅된 카지노 인덱스
 
-		//배팅한 플레이어 수 찾기
-		//let btPlayerNum = (btPlayer.length); // 배팅된 수
-		bettingN = $(this).index()+1; // 배팅된 카지노
-		console.log(bettingN);
-
-		if(btPlayerNum==0) {
+		if(btPlayerNum===0) {
 			btPlayerNum++;
-			
 			$(this).append('<span class="btPlayer">배팅' + btPlayerNum + '</span>');
 			$(this).addClass('player1');
 			btIndex1 = bettingN;
-			player1.bt = bettingTotal - betting;
 			$('#player1 .score').text(player1.bt);
-			//console.log(btIndex1);
-		}else if(btPlayerNum==1) {
+		}else if(btPlayerNum===1) {
 			btPlayerNum++;
 			$(this).append('<span class="btPlayer">배팅' + btPlayerNum + '</span>');
 			$(this).addClass('player2');
 			btIndex2 = bettingN;
-			player2.bt = bettingTotal - betting;
-			$('#player2 .score').text(player1.bt);
-			//console.log(btIndex2);
-			//bettingTotal = bettingTotal - betting;
-			
+			$('#player2 .score').text(player2.bt);
 		}else{
 			alert('배팅 끝');
 		}
-		
 	});
-	
 }
 
-//주사위 굴리기
+// 주사위 굴리기
 function throwDice() {
 	let randomdice=(Math.floor(Math.random()*6)+1);
-	//console.log(randomdice);
 	$('.diceNum').text(randomdice);
-	//return randomdice;
-	
+
 	if(randomdice == btIndex1){
-		console.log('배팅1 같음');
+		console.log('배팅1 당첨');
 		player1.bt += betting;
 		player2.bt -= betting;
 		$('.player1').addClass('on1');
 		$('.player2').removeClass('on2');
 
 	}else if(randomdice == btIndex2){
-		console.log('배팅2 같음');
+		console.log('배팅2 당첨');
 		player1.bt -= betting;
 		player2.bt += betting;
 		$('.player1').removeClass('on1');
 		$('.player2').addClass('on2');
 	}
-	
+
 	$('#player1 .score').text(player1.bt); 
 	$('#player2 .score').text(player2.bt);
+	
+}
+
+// 게임 재시작(초기화)
+function reStart() {
+	bettingTotal = 100000; //최초 배당 금액 다시 세팅
+	player1.bt = 100000;
+	player2.bt = 100000;
+	btPlayerNum = 0; //배팅한 플레이어 초기화
+
+	$('.diceNum').text(''); //배팅 초기화
+
+	$('#player1 .score').text(player1.bt);
+	$('#player2 .score').text(player2.bt);
+
+	$('.player1').removeClass('on1');
+	$('.player2').removeClass('on2');
+	$('.gAreaIn li').removeClass();
+	$('.gAreaIn li .btPlayer').remove();
+
+	$('.reStart').hide();
+	$('.startDice').show();
 	
 }
 
@@ -94,9 +98,8 @@ vegas.gameS = function(){
 
 	// 게임 시작
 	$('.startBtn').click(function() {
-		bettingS(bettingTotal,player); // 최초 배당 금액 배분(10만불)
+		bettingS(bettingTotal); // 최초 배당 금액 배분(10만불)
 		$(this).hide(); // 시작 버튼 감추기
-
 	});
 
 	// 주사위 굴리기
@@ -112,7 +115,7 @@ vegas.gameS = function(){
 				alert('player2 승리!');
 			}
 
-			alert('게임 끝');
+			alert('게임 끝!');
 			$(this).hide();
 			$('.reStart').show(); //재시작 버튼
 
@@ -130,24 +133,4 @@ vegas.gameS = function(){
 }
 
 
-// 게임 재시작...
-function reStart() {
-	bettingTotal = 100000; //최초 배당 금액 다시 세팅
-	player1.bt = 100000;
-	player2.bt = 100000;
-	btPlayerNum = 0;
 
-	$('.diceNum').text(''); //배팅 초기화
-
-	$('#player1 .score').text(player1.bt);
-	$('#player2 .score').text(player2.bt);
-
-	$('.player1').removeClass('on1');
-	$('.player2').removeClass('on2');
-	$('.gAreaIn li').removeClass();
-	$('.gAreaIn li .btPlayer').remove();
-
-	$('.reStart').hide();
-	$('.startDice').show();
-	
-}
